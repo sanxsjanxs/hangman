6da99544cb553fb595c4362fcee920ab0057a0ab
sanxsjanxs <sanxsjaxns@gmail.com>
2018-10-24 16:23:04 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+def hangman():
+    wrong = 0 #間違えた回数
+    stages = ["",
+              "________        ",
+              "|               ",
+              "|        |      ",
+              "|        0      ",
+              "|       /|\     ",
+              "|       / \     ",
+              "|               "
+    ]
+    word_l = ["cat","dog","bird"]
+    word = word_l[random.randint(0,2)]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け!正解は {}.".format(word))
+
+hangman()
+
